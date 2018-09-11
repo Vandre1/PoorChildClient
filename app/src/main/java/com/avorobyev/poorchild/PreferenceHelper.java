3b1f96d8fca4b2c547aa7ad91a72dffbb647972d
Андрей Воробьев <avorobyev@1d61.com>
2018-09-11 21:23:59 +0300

    test2

--- app/src/main/java/com/avorobyev/poorchild/PreferenceHelper.java
+++ app/src/main/java/com/avorobyev/poorchild/PreferenceHelper.java
@@ -6,5 +6,4 @@ public class PreferenceHelper {
     public static final String ParentDeviceId = "PARENT_DEVICE_ID";
     public static final String IsCurrentDeviceChild = "IS_CURRENT_DEVICE_CHILD";
     public static final String IsCurrentDeviceParent = "IS_CURRENT_DEVICE_PARENT";
-
 }
